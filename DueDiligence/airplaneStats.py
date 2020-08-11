1b9fda633a650b3d283c1abd383f1e11c3224fd1
Matt Lee <mattlee@matt-lees-mbp.attlocal.net>
2020-08-11 13:58:33 -0700

    stuff

---
+++ DueDiligence/airplaneStats.py
@@ -0,0 +1,37 @@
+'''
+For the 2d lists below the first index is the airplanes name, the 2nd index
+is the capacity of the plane and the 3rd index is the miles per gallon per
+seat of the airplane.
+'''
+
+NAME = 0
+SEATS = 1
+MPG_PER = 2
+
+regionalAirliners = [['Airbus A321neo', 192, 107.4],
+                     ['Airbus A220-300', 160, 105],
+                     ['Airbus A320neo', 154, 104.7],
+                     ['Boeing 737 MAX 9', 180, 103],
+                     ['Boeing 737 MAX 8', 166, 103.2],
+                     ['Boeing 737-900ER', 180, 88]]
+
+shortHaulAirliners = [['Airbus A220-300', 135, 127],
+                      ['Airbus A319neo', 136, 122],
+                      ['Airbus A220-100', 125, 103],
+                      ['Boeing 737 MAX 7', 140, 121],
+                      ['Boeing 737 MAX 9', 180, 116],
+                      ['Boeing 737 MAX 8', 162, 115]]
+
+mediumHaulAirliners = [['Airbus A330neo-900', 310, 97],
+                       ['Airbus A320', 150, 97],
+                       ['Airbus A321neoLR', 154, 97],
+                       ['Boeing 737 MAX 8', 168, 110],
+                       ['Boeing 787-8', 291, 104],
+                       ['Boeing 787-9', 304, 99]]
+
+longHaulAirliners = [['Airbus A350-900', 315, 98],
+                     ['Airbus A330neo-900', 300, 95],
+                     ['Airbus A330neo-800', 248, 86],
+                     ['Boeing 787-9', 304, 102],
+                     ['Boeing 777-9X', 395, 97],
+                     ['Boeing 787-8', 243, 85]]
